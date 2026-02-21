d70d74f85499265c54fc417158a56657af4110b0
Mike Moseley <mike.moseley@tuta.io>
2026-02-21 01:57:31 -0500

    Finished CH2:L6

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -15,15 +15,33 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(node, node2)
 
     def test_text_neq(self):
-        node = TextNode("Text", TextType.PLAIN)
-        node2 = TextNode("Different", TextType.PLAIN)
+        node = TextNode("Text", TextType.TEXT)
+        node2 = TextNode("Different", TextType.TEXT)
         self.assertNotEqual(node, node2)
 
     def test_type_neq(self):
-        node = TextNode("Text", TextType.PLAIN)
+        node = TextNode("Text", TextType.TEXT)
         node2 = TextNode("Text", TextType.ITALIC)
         self.assertNotEqual(node, node2)
 
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
 
+    def test_url(self):
+        node = TextNode("This is a text node", TextType.LINK, "archlinux.org")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "This is a text node")
+        self.assertEqual(html_node.props["href"], "archlinux.org")
+    def test_img(self):
+        node = TextNode("Boots", TextType.IMAGE, "https://www.boot.dev/_nuxt/new_boots_profile.DriFHGho.webp")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, None)
+        self.assertEqual(html_node.props["alt"], "Boots")
+        self.assertEqual(html_node.props["src"], "https://www.boot.dev/_nuxt/new_boots_profile.DriFHGho.webp")
 if __name__ == "__main__":
     unittest.main()
