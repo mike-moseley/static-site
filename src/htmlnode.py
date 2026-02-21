df9c1fc2b2e0a235917cd4d405a5e7110aa3dc25
Mike Moseley <mike.moseley@tuta.io>
2026-02-20 22:17:36 -0500

    Finished CH2:L4

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -26,3 +26,31 @@ class HTMLNode:
         prop_bool = self.props == other.props
 
         return tag_bool and value_bool and children_bool and prop_bool
+
+class LeafNode(HTMLNode):
+    def __init__(self, value, tag, props=None):
+        super().__init__(value, tag, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("Leaf nodes must have a value.")
+        if self.tag is None:
+            return self.value
+        string = ""
+        match self.tag:
+            case "p" | "b" | "i" | "div":
+                string = f'<{self.tag}>{self.value}</{self.tag}>'
+            case "a":
+                href = ""
+                if self.props is not None:
+                    href = self.props["href"]
+                string = f'<{self.tag} href="{href}">{self.value}</{self.tag}>'
+            case "img":
+                src = ""
+                if self.props is not None:
+                    src = self.props["src"]
+                string = f'<{self.tag} src="{src}" alt="{self.value}">'
+        return string
+
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,7 @@
 import unittest
 
 from htmlnode import HTMLNode
+from htmlnode import LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_eq(self):
@@ -29,3 +30,19 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode("a", "This is a great website", None, {"href": "https://www.boot.dev"})
         node2 = HTMLNode("a", "This is a great website", None, {"href": "https://archlinux.org"})
         self.assertNotEqual(node, node2)
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_div(self):
+        node = LeafNode("div", "Hello, world!")
+        self.assertEqual(node.to_html(), "<div>Hello, world!</div>")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.boot.dev"})
+        self.assertEqual(node.to_html(), '<a href="https://www.boot.dev">Click me!</a>')
+
+    def test_leaf_to_html_img(self):
+        node = LeafNode("img", "Boots", {"src": "https://www.boot.dev/_nuxt/new_boots_profile.DriFHGho.webp"})
+        self.assertEqual(node.to_html(), '<img src="https://www.boot.dev/_nuxt/new_boots_profile.DriFHGho.webp" alt="Boots">')
